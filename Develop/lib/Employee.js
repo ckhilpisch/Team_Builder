1a1aa8a9fb5fe141537c6eedc0540c4d696e84ef
CaroKris <ckhilpisch@gmail.com>
2021-02-19 12:24:47 -0600

    adding a class to employee and exporting the class to all of the subclasses

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,16 @@
+class Employee {
+    constructor(name, role, id, email) {
+        this.name = name;
+        this.id = id;
+        this.role = role;
+        this.email = email;
+    }
+}
+
+module.exports = Employee;
+
+
+
+
+
+// TODO: Write code to define and export the Employee class
